6c13a3476a5c585ca3c26f99d82c32aa588d8848
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-15 08:42:45 +0100

    Exercici 1 Javascript Lògica

---
+++ RA3.JavaScript - Logica.js
@@ -0,0 +1,10 @@
+var numero = 14
+if(numero % 3===0){
+    console.log("Es multiple de 3")
+}
+else if(numero % 7===0){
+    console.log("Es multiple de 7")
+}
+else{
+    console.log("No es multiple de 3 ni de 7")
+}
