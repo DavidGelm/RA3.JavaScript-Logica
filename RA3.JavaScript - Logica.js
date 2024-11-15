0ae176add5966c818203547f9f0ee687d646db79
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-15 09:38:29 +0100

    Exercici 3 Javascript Lògica

--- RA3.JavaScript - Logica.js
+++ RA3.JavaScript - Logica.js
@@ -1,8 +1,20 @@
-var num1 = 50
-var num2 = 9
-
-num1 >=10 && num1 <=50  ? console.log("Esta en el rang el numero: "+num1)
-                        : console.log("No esta en el rang el numero: "+num1)
-num2 >=10 && num2 <=50  ? console.log("Esta en el rang el numero: "+num2 )
-                        : console.log("No esta en el rang el numero: " +num2 )
+var nota = 9
+if(nota<=4.99){
+    console.log("Suspés")
+}
+else if(nota>4.99 && nota<=5.99){
+    console.log("Suficient")
+}
+else if(nota>5.99 && nota<=6.99){
+    console.log("Bé")
+}
+else if(nota>6.99 && nota <=8.99){
+    console.log("Notable")
+}
+else if(nota > 8.99 && nota <=10){
+    console.log("Excel·lent")
+}
+else{
+    console.log("Nota no valida")
+}
 
