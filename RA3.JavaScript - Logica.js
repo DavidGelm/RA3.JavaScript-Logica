00c7b4bcbed05fb97fa8f4972877127813116440
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-15 09:21:38 +0100

    Exercici 2 Javascript Lògica

--- RA3.JavaScript - Logica.js
+++ RA3.JavaScript - Logica.js
@@ -1,10 +1,8 @@
-var numero = 14
-if(numero % 3===0){
-    console.log("Es multiple de 3")
-}
-else if(numero % 7===0){
-    console.log("Es multiple de 7")
-}
-else{
-    console.log("No es multiple de 3 ni de 7")
-}
+var num1 = 50
+var num2 = 9
+
+num1 >=10 && num1 <=50  ? console.log("Esta en el rang el numero: "+num1)
+                        : console.log("No esta en el rang el numero: "+num1)
+num2 >=10 && num2 <=50  ? console.log("Esta en el rang el numero: "+num2 )
+                        : console.log("No esta en el rang el numero: " +num2 )
+
