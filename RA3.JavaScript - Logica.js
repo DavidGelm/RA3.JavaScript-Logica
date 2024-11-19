4124fc46e10364a812b284de7af3b98f64819c5c
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-19 17:44:10 +0100

    Exercici 6 Javascript Lògica

--- RA3.JavaScript - Logica.js
+++ RA3.JavaScript - Logica.js
@@ -1,10 +1,10 @@
-let numero = 15;
-let n = 0;
-let suma = 0;
-
-for (let i = 1; suma + i <= numero; i++) {
-    n = i;
-    suma += i;
+let cadena = "hola3pat2ta"
+let suma = 0
+for (let i = 0; i< cadena.length; i++){
+    if (!isNaN(cadena[i]) && cadena[i] !== ''){
+        suma += parseInt(cadena[i])
+    }
 }
 
-console.log("El numero maxim es: " + n);
+console.log("La suma dels numeros del string es: " + suma)
+
