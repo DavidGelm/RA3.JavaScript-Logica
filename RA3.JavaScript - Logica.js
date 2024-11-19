17a48ec9bc54148e67fd7880e92115de1f4b86d1
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-19 17:53:07 +0100

    Exercici 8 Javascript Lògica

--- RA3.JavaScript - Logica.js
+++ RA3.JavaScript - Logica.js
@@ -1,12 +1,9 @@
-const numeros = [3,1,9]
-let contador
-for(let i = 0;i < numeros.length;i++){
-    for(let j = 0;j < numeros.length-i-1; j++){
-        if(numeros[j] < numeros[j+1]){
-            contador = numeros[j]
-            numeros[j] = numeros[j+1]
-            numeros[j+1] = contador
-        }
+const numero = 18
+for(let i =0;i<=numero;i++){
+    if(i%2===0){
+        console.log(i + " Parell")
+    }
+    else{
+        console.log(i + " Senar")
     }
 }
-console.log("Llista ordenada major a menor: " + numeros)
