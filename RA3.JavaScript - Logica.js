61bf3b2e85590536fc455585eb5cb19a59d74c3b
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-19 17:58:08 +0100

    Exercici 9 Javascript Lògica

--- RA3.JavaScript - Logica.js
+++ RA3.JavaScript - Logica.js
@@ -1,9 +1,10 @@
-const numero = 18
-for(let i =0;i<=numero;i++){
-    if(i%2===0){
-        console.log(i + " Parell")
-    }
-    else{
-        console.log(i + " Senar")
+function patro(n){
+    for(let i = 1;i <= n;i++){
+        let linia = ''
+        for(let j = 1;j<=i;j++){
+            linia += " *"
+        }
+        console.log(linia)
     }
 }
+patro(5)
